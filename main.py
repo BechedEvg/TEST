0ee7ec38e2e5ad4140ead45abae31c83f696ccd4
BechedEvg <65180659+BechedEvg@users.noreply.github.com>
2023-03-17 22:57:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from typing import re
-
+import openpyxl
 from bs4 import BeautifulSoup
 from time import sleep
 import requests
@@ -31,7 +31,13 @@ class Exel_RW:
         workbook = pd.read_excel(file_name)
         list_product = []
         for elements in workbook.values:
-            list_product.append(list(elements))
+            elements = list(elements)
+            elements2 = []
+            for i in elements:
+                if type(i) == str:
+                    i = i.replace('\xa0', ' ')
+                elements2.append(i)
+            list_product.append(elements2)
         return list_product
 
     def write_exel(write_lists, file_name, sheet_name=0):
@@ -39,7 +45,7 @@ class Exel_RW:
             workbook = Workbook()
             workbook.save(file_name)
             workbook.close()
-        workbook = load_workbook(file_name)
+        workbook = load_workbook(file_name, data_only=True)
         if sheet_name == 0:
             worksheet = workbook[workbook.sheetnames[0]]
         elif sheet_name not in workbook.sheetnames:
@@ -55,15 +61,27 @@ class Exel_RW:
 
 class Analysis:
 
-    def __init__(self, list_data, price_original):
+    def __init__(self, list_data, list_original):
         self.list_data = list_data
-        self.price_original = price_original
+        self.list_original = list_original
 
     def price_check(self):
-        pass
-
-    def difference_calculation(self):
-        pass
+        price_data = int(self.list_data[2])
+        price_original = int(self.list_original[0])
+        if price_original < price_data:
+            percentage_value = int(abs(((price_original - price_data) / price_data) * 100)) # если цена аналога больше
+            if percentage_value > 30:
+                return False
+            else:
+                return str(percentage_value)
+        elif price_original > price_data:
+            percentage_value = int(abs(((price_data - price_original) / price_original) * 100))  # если цена аналога меньше
+            if percentage_value > 50:
+                return False
+            else:
+                return f"-{percentage_value}"
+        else:
+            return 0
 
 
 def get_legacy_session():
@@ -75,7 +93,11 @@ def get_legacy_session():
 
 
 def get_html(url):
-    html = get_legacy_session().get(url)
+    try:
+        html = get_legacy_session().get(url, timeout=10, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0'})
+    except:
+        sleep(2)
+        html = get_html(url)
     return html
 
 
@@ -92,7 +114,10 @@ def get_emex_original_list_product(vendor_cod):
     if availability != None:
         regex_num = re.compile('\d+')
         rating = parser.find(class_="sc-b0f3936c-1 kHZHVQ").text
-        quantity = "".join(regex_num.findall(parser.find(class_="sc-d67ce909-11 sc-d67ce909-13 fuNkfc csqgZG").text))
+        try:
+            quantity = "".join(regex_num.findall(parser.find(class_="sc-d67ce909-11 sc-d67ce909-13 fuNkfc csqgZG").text))
+        except:
+            quantity = "под заказ"
         delivery = "".join(regex_num.findall(parser.find(class_="sc-d67ce909-11 sc-d67ce909-14 fuNkfc jtgcED").text))
         price = "".join(regex_num.findall(parser.find(class_="sc-d67ce909-11 sc-d67ce909-15 fuNkfc gXBVKh").text))
         list_product.append(price)
@@ -130,10 +155,10 @@ def get_lists_product(input_lists):
 
 # в конце цыкла перебора аналогов(lists_dict_analogs) просто плюсуем list_original_product + list_analog, а добовление в write_list уже
 # делаем поле выполнения цикла
-    count = len(input_lists)
-    for list_product in input_lists[:3]:
+    count = len(input_lists[3000:])
+    for list_product in input_lists[3000:]:
         print(list_product)#########################
-        sleep(2)
+
         print(count)################################
         count -= 1##############################################
 
@@ -146,31 +171,99 @@ def get_lists_product(input_lists):
 
         if vendor_cod not in ["nan", "-"]:
 
-            print(vendor_cod)###################################################
+            emex_list_original_product = get_emex_original_list_product(vendor_cod)
 
-            dict_product = get_emex_dict_products(vendor_cod)
-            lists_dict_analogs = get_lists_dict_analogs(dict_product)
+            if emex_list_original_product:         # если продукт найден
+                list_original_product += emex_list_original_product
 
-            counter_analog = 0
-            for dict_analog in lists_dict_analogs:
-                #if counter_analog != 5:
-                    #counter_analog += 1
-                    if dict_analog["quantity"] == 1000:
-                        dict_analog["quantity"] = "под заказ"
-                    list_analog = [dict_analog['make'],
-                                   dict_analog['vendor_cod'],
-                                   dict_analog['price'],
-                                   f"https://emex.ru/products/{dict_analog['vendor_cod']}/{dict_analog['make']}/29241",
-                                   dict_analog["rating"],
-                                   dict_analog["quantity"],
-                                   dict_analog["delivery"],
-                                   ]
-                    list_original_product += list_analog
-                else:
-                    #counter_analog = 0
-                    break
-            write_list.append(list_original_product)
+                print(vendor_cod)###################################################
+
+                dict_product = get_emex_dict_products(vendor_cod)
+                lists_dict_analogs = get_lists_dict_analogs(dict_product)
+
+                counter_analog = 0
+
+                for dict_analog in lists_dict_analogs:
+
+                    if counter_analog != 5:
+
+
+                        if dict_analog["quantity"] == 1000:
+                            dict_analog["quantity"] = "под заказ"
+                        list_analog = [dict_analog['make'],
+                                       dict_analog['vendor_cod'],
+                                       dict_analog['price'],
+                                       f"https://emex.ru/products/{dict_analog['vendor_cod']}/{dict_analog['make']}/29241",
+                                       dict_analog["rating"],
+                                       dict_analog["quantity"],
+                                       dict_analog["delivery"],
+                                       ]
+
+                        check_by_criterion = Analysis(list_analog, emex_list_original_product).price_check()
+                        if check_by_criterion:
+                            counter_analog += 1
+
+                            list_original_product += list_analog + [check_by_criterion]
+                    else:
+                        counter_analog = 0
+                        break
+                write_list.append(list_original_product)
     return write_list
+
+
+def write_list_data(lists_product):
+    column_names = [["ID",
+                     "Марка ТС",
+                     "Модель ТС",
+                     "Тип кузова",
+                     "Ценовой сегмент",
+                     "Наименование запчасти",
+                     "Артикул (оригинал)",
+                     "Цена EMEX на артикул оригинала",
+                     "Рейтинг",
+                     "Наличие в шт.",
+                     "Количество дней доставки",
+                     "Бренд Аналога  1",
+                     "Артикул Аналога 1",
+                     "Цена Аналога 1",
+                     "Источник цен от emex (ссылка)",
+                     "Рейтинг",
+                     "Наличие в шт.",
+                     "Количество дней доставки",
+                     "% разница стоимости",
+                     "Бренд Аналога  2",
+                     "Артикул Аналога 2",
+                     "Цена Аналога 2",
+                     "Источник цен от emex (ссылка)",
+                     "Рейтинг",
+                     "Наличие в шт.",
+                     "Количество дней доставки",
+                     "% разница стоимости",
+                     "Бренд Аналога  3",
+                     "Артикул Аналога 3",
+                     "Цена Аналога 3",
+                     "Источник цен от emex (ссылка)",
+                     "Рейтинг",
+                     "Наличие в шт.",
+                     "Количество дней доставки",
+                     "% разница стоимости",
+                     "Бренд Аналога  4",
+                     "Артикул Аналога 4",
+                     "Цена Аналога 4",
+                     "Источник цен от emex (ссылка)",
+                     "Рейтинг",
+                     "Наличие в шт.",
+                     "Количество дней доставки",
+                     "% разница стоимости",
+                     "Бренд Аналога  5",
+                     "Артикул Аналога 5",
+                     "Цена Аналога 5",
+                     "Источник цен от emex (ссылка)",
+                     "Рейтинг",
+                     "Наличие в шт.",
+                     "Количество дней доставки",
+                     "% разница стоимости",]]
+    return column_names + lists_product
 
 
 def get_emex_dict_products(vendor_cod):
@@ -185,13 +278,19 @@ def main():
     input_list = Exel_RW.read_exel("input.xlsx")
 
     product_lists = get_lists_product(input_list)
+    if "korzina.xlsx" not in os.listdir():
+        product_lists = write_list_data(product_lists)
     Exel_RW.write_exel(product_lists, "korzina.xlsx")
 
 
-
 if __name__ == '__main__':
-    main()
+    #main()
     pass
 
-rez = get_emex_original_list_product("63219853369")
-print(rez)
+#rez = get_emex_original_list_product("31424809")1358901gg
+#rezB = ((111904 - 124186) / 124186) * 100 #если цена аналога больше
+#rezM = ((5755 - 4041) / 4041) * 100 #если цена аналога меньше
+
+#print(rezB)
+input_list = Exel_RW.read_exel("input.xlsx")
+print(input_list[2999:3000])
